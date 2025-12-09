63fd5a877b9c0499e365b27299029909a8b312ac
João Coutinho <joaoccoutinho1993@gmail.com>
2025-12-09 12:36:31 -0300

    Refactor: Adiciona tipagem e documentação no módulo de análise

--- analise.py
+++ analise.py
@@ -1,10 +1,17 @@
 # Arquivo: analise.py
 
-def calcular_total(lista_generica):
+def calcular_total(lista_generica: list) -> float:
+    """
+    Calcula o valor total do estoque baseado no preço e quantidade.
+
+    Args:
+        lista_de_produtos (list): Lista de dicionários contendo 'preco' e 'qtd'.
+
+    Returns:
+        float: O valor total somado de todos os itens.
+    """
     total = 0
     for item in lista_generica:
-        # Note que a gente confia que quem chamar vai mandar 
-        # uma lista de dicionários com "preco" e "qtd"
         subtotal = item["preco"] * item["qtd"]
         total = total + subtotal
     
